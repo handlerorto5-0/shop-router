1b2a9f8d60dc165d9683ebaf87a34c985ac27958
kv-2 <str_token@mail.ru>
2019-07-01 14:53:25 +0300

    add controller the Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/ping'
+  get 'home/pong'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
