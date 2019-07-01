7156c84630cd4f9dc5b0adb12fef5701b78adc40
kv-2 <str_token@mail.ru>
2019-07-01 15:14:23 +0300

    define my path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'home/ping'
+  get 'home/ping', as: 'defferent_name'
   get 'home/pong'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
