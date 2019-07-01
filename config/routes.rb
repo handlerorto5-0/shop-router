5ee3488c93f34f6cc58460b51101816484c5c341
kv-2 <str_token@mail.ru>
2019-07-01 15:24:20 +0300

    define another destination

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'home/ping', as: 'defferent_name'
-  get 'home/pong'
+  get 'home/applepie', to: "home#ping"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
